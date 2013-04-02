e75a6a0e4d44304843884c97daf6ec104565e354
adl2158 <adl2158@rit.edu>
2013-04-02 15:09:03 -0700

    Added send and status facebook scripts

---
+++ Web Engineering 549/Web Engineering 549/Scripts/Facebook/updateStatus.js
@@ -0,0 +1,17 @@
+﻿
+        FB.init({ appId: "449592011787745", status: true, cookie: true });
+
+        function postToFeed() {
+
+            // calling the API ...
+            var obj = {
+                method: 'feed',
+            };
+
+            function callback(response) {
+                document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
+            }
+
+            FB.ui(obj, callback);
+        }
+   
