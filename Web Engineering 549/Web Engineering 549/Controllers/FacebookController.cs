b9508f4590413aed577e6028b9909be31b89a6d4
adl2158 <adl2158@rit.edu>
2013-04-02 15:07:42 -0700

    Added FacebookController

---
+++ Web Engineering 549/Web Engineering 549/Controllers/FacebookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Engineering_549.Controllers
+{
+    public class FacebookController : Controller
+    {
+        //
+        // GET: /Facebook/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Facebook()
+        {
+            return View();
+        }
+
+    }
+}
