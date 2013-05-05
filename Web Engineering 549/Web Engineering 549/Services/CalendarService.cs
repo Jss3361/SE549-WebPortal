4c7e37d37f4ca638ad94184c9217f1ecb1e825f6
adl2158 <adl2158@rit.edu>
2013-05-05 14:28:59 -0700

    calendar service

---
+++ Web Engineering 549/Web Engineering 549/Services/CalendarService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web_Engineering_549.Models;
+using Web_Engineering_549.Database;
+
+namespace Web_Engineering_549.Services
+{
+    public class CalendarService
+    {
+
+        public bool SaveEvent(CalendarEvent calendar_event)
+        {
+            try
+            {
+                using (var context = new EntityContext())
+                {
+                    context.CalendarEvent.Add(calendar_event);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        public List<CalendarEvent> getEvents(long userid)
+        {
+
+            List<CalendarEvent> events = new List<CalendarEvent>();
+            try
+            {
+                using (var context = new EntityContext())
+                {
+                    events = context.CalendarEvent.Where(p => p.user_id == userid).ToList();
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return events;
+        }
+
+
+
+
+
+    }
+}
