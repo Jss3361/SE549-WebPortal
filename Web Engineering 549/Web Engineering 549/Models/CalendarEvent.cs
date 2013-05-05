eee18b6d83c8013c93dc09e1070d7205e0e48d6e
adl2158 <adl2158@rit.edu>
2013-05-05 14:28:17 -0700

    Calendar model

---
+++ Web Engineering 549/Web Engineering 549/Models/CalendarEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_Engineering_549.Models
+{
+    public class CalendarEvent
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public System.Int32 event_id { get; set; }
+        public String title { get; set; }
+        public DateTime start_date { get; set; }
+        public System.Int64 user_id { get; set; }
+        public String description { get; set; }
+        public String location { get; set; }
+    }
+}
